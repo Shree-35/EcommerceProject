ff5f2ad754edfa8e57122e3f6382cb03988c5855
Shree Harsh A <146053218+Shree-35@users.noreply.github.com>
2025-03-20 15:51:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar(){
+  sidenav.style.left="0"
+
+}
+function closeNavbar(){
+  sidenav.style.left="-60%"
+
+}
+
+/*<div class="contact-bar">
+<form class="contact-search">
+<input type="text" id="name" placeholder="name">
+ <input type="text" id="email" placeholder="email">
+
+<input type="number" id="contact" placeholder="">
+</form>
+ </div>*/
+
